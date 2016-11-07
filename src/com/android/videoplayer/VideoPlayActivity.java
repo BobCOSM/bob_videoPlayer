39fff990ad61ab2765f72c0dd0e9a52dce5b3374
bob_captain <bob_captain@163.com>
2016-11-07 18:55:55 +0800

    start design play video part

--- src/com/android/videoplayer/VideoPlayActivity.java
+++ src/com/android/videoplayer/VideoPlayActivity.java
@@ -2,15 +2,22 @@ package com.android.videoplayer;
 
 import java.io.IOException;
 
+import com.android.videocontroler.VideoController;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.media.AudioManager;
+import android.media.AudioManager.OnAudioFocusChangeListener;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
 public class VideoPlayActivity extends Activity {
+	
+	private VideoController mVideoController = null;
 	
 	private MediaPlayer mMediaPlayer = null;
 	private SurfaceView mSurfaceView = null;
@@ -18,6 +25,14 @@ public class VideoPlayActivity extends Activity {
 	
 	private int mLastPlayPosition = 0;
 	private String mLastPlayVideoPath = "";
+	private String mPlayPath = "";
+	private int mCurrentPosition = 0;
+	private AudioManager mAudioManager = null;
+	private AudioFocuseChangeListener mAudioFocuseChangeListener = null;
+
+	private boolean mIsAudioFocus = false;
+	private boolean mIsPauseByUser = false;
+	private boolean mIsPlaying = false;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -26,6 +41,25 @@ public class VideoPlayActivity extends Activity {
 		mSurfaceView = (SurfaceView)findViewById(R.id.surface_view);
 		mSurfaceHolder = mSurfaceView.getHolder();
 		mSurfaceHolder.addCallback(new surfaceCallback());
+		initController();
+	}
+	
+	private void initController(){
+		mVideoController = mVideoController.getInstance();
+	}
+	
+	
+	private void requestAudioFouse(){
+		if(mAudioManager == null){
+			mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+		}
+		if(mAudioFocuseChangeListener == null){
+			mAudioFocuseChangeListener = new AudioFocuseChangeListener();
+		}
+		if(!mIsAudioFocus){
+			mAudioManager.requestAudioFocus(mAudioFocuseChangeListener, 
+					AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+		}
 	}
 	
 	private void initMediaPlayer(){
@@ -36,6 +70,7 @@ public class VideoPlayActivity extends Activity {
 		mMediaPlayer.setDisplay(mSurfaceHolder);
 		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 		setMediaPlayerListener();
+		requestAudioFouse();
 	}
 	
 	private void setMediaPlayerListener(){
@@ -43,7 +78,7 @@ public class VideoPlayActivity extends Activity {
 			@Override
 			public void onPrepared(MediaPlayer mp) {
 				// TODO Auto-generated method stub
-				
+				mVideoController.setPlayState(mPlayPath);
 			}
 		});
 		
@@ -66,12 +101,13 @@ public class VideoPlayActivity extends Activity {
 		});
 	}
 	
-	private void playByUrl(Uri videoUri){
+	private void playVideo(Uri videoUri){
 		String videoStr = videoUri.getPath();
-		playByPath(videoStr);
+		playVideo(videoStr);
 	}
 	
-	private void playByPath(String videoPath){
+	private void playVideo(String videoPath){
+		mPlayPath = videoPath;
 		try {
 			mMediaPlayer.setDataSource(videoPath);
 		} catch (IllegalArgumentException | SecurityException
@@ -82,6 +118,54 @@ public class VideoPlayActivity extends Activity {
 		mMediaPlayer.prepareAsync();
 	}
 	
+	private void play(){		//继续播放
+		mMediaPlayer.start();
+	}
+	
+	private void pause(){		//停止播放 
+		mMediaPlayer.pause();
+	}
+	
+	private void next(){		//播放下一个
+	}
+	
+	private void previous(){	//播放上一个
+		
+	}
+
+	private void playCurrentVideo(){
+		//mPlayPath  mCurrentPosition
+	}
+	
+	public void onBack(View view){
+		this.finish();
+	}
+	
+	public void onHome(View view){
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		startActivity(intent);
+	}
+	
+	public void onPlayOrPause(View view){
+		if(mIsPlaying){
+			mIsPauseByUser = true;
+			mIsPlaying = false;
+			pause();
+		} else {
+			mIsPauseByUser = false;
+			mIsPlaying = false;
+			play();
+		}
+	}
+	
+	public void onPlayNext(View view){
+		
+	}
+	
+	public void onPlayPrevious(View view){
+		
+	}
 	class surfaceCallback implements SurfaceHolder.Callback{
 
 		@Override
@@ -99,6 +183,25 @@ public class VideoPlayActivity extends Activity {
 		public void surfaceDestroyed(SurfaceHolder holder) {
 			// TODO Auto-generated method stub
 			
+		}
+	}
+	class AudioFocuseChangeListener implements OnAudioFocusChangeListener{
+
+		@Override
+		public void onAudioFocusChange(int focusChange) {
+			// TODO Auto-generated method stub
+			switch(focusChange){
+			case AudioManager.AUDIOFOCUS_LOSS: 	//失去音频焦点
+				mIsAudioFocus = false;
+				pause();
+				break;
+			case AudioManager.AUDIOFOCUS_GAIN:	//获得音频焦点
+				mIsAudioFocus = true;
+				if(!mIsPauseByUser){
+					play();
+				}
+				break;
+			}
 		}
 	}
 }
